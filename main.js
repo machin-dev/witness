ccb1b979bce6f0d243b3ffed1a8263f43c8f46e7
Louis Machin <ldhmachin@gmail.com>
2020-11-13 06:52:51 +0000

    Updated values

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function createWindow () {
 
   // if main window is ready to show, then destroy the splash window and show up the main window
   async function closeSplash() {
-    await sleep(0);
+    await sleep(3200);
     splash.destroy();
     mainWindow.show();
   };
